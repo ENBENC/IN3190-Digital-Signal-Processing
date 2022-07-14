8b60de682c7439810ac07d3d35a1586d7b8ad254
BIHUI CHEN <62288436+ENBENC@users.noreply.github.com>
2022-07-14 18:13:42 +0200

    Delete delta.m

--- Oblig1 - Signalbehandling i Seismikk/delta.m
+++
@@ -1,7 +0,0 @@
-function d = delta(n,x)
-%Dette funksjon ta imot en n vector og en integer x
-%Return en vektor paa lengde n, der d(x+1) er 1 og resten er 0.
-d = zeros(1,length(n));
-%siden matlab har array index fra 1 dermed maa vi gjoere en variabel skift.
-d(x+1) = 1;
-end
